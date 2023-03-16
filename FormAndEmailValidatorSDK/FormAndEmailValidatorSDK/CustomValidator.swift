f19fd526c8e4d22b6b04dbb85433392aa2d01607
Satyam Dixit <satyam.d@antino.io>
2023-03-16 19:27:13 +0530

    SDKAdded

---
+++ FormAndEmailValidatorSDK/FormAndEmailValidatorSDK/CustomValidator.swift
@@ -0,0 +1,36 @@
+//
+//  CustomValidator.swift
+//  FormAndEmailValidatorSDK
+//
+//  Created by satyam dixit on 16/03/23.
+//
+
+import Foundation
+
+public struct FormAndEmailValidator {
+    //MARK: - EmailValidation
+    
+    public static func isValidEmailAddress(emailAddressString: String) -> Bool {
+      
+      var returnValue = true
+      let emailRegEx = "[A-Z0-9a-z.-_]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,3}"
+      
+      do {
+          let regex = try NSRegularExpression(pattern: emailRegEx)
+          let nsString = emailAddressString as NSString
+          let results = regex.matches(in: emailAddressString, range: NSRange(location: 0, length: nsString.length))
+          
+          if results.count == 0
+          {
+              returnValue = false
+          }
+          
+      } catch let error as NSError {
+          print("invalid regex: \(error.localizedDescription)")
+          returnValue = false
+      }
+      
+      return  returnValue
+    }
+
+}
---
+++ FormAndEmailValidatorSDK/FormAndEmailValidatorSDK/FormAndEmailValidatorSDK.h
@@ -0,0 +1,18 @@
+//
+//  FormAndEmailValidatorSDK.h
+//  FormAndEmailValidatorSDK
+//
+//  Created by satyam dixit on 16/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FormAndEmailValidatorSDK.
+FOUNDATION_EXPORT double FormAndEmailValidatorSDKVersionNumber;
+
+//! Project version string for FormAndEmailValidatorSDK.
+FOUNDATION_EXPORT const unsigned char FormAndEmailValidatorSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FormAndEmailValidatorSDK/PublicHeader.h>
+
+
